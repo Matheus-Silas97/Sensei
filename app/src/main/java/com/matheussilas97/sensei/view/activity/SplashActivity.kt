7b3a747472066e85c1964ccf665b7091459db75a
Matheus-Silas97 <matheus.silas123@gmail.com>
2021-02-15 17:52:12 -0300

    Initial commit

---
+++ app/src/main/java/com/matheussilas97/sensei/view/activity/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.matheussilas97.sensei.view.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.matheussilas97.sensei.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 9000 // 1 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
