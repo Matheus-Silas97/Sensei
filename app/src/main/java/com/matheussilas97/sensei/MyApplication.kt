472bbe3ab2421e3b88ac73d0c4b7d9b8693569a5
Matheus-Silas97 <matheus.silas123@gmail.com>
2021-03-23 00:34:29 -0300

    Disabled dark mode.

--- app/src/main/java/com/matheussilas97/sensei/MyApplication.kt
+++ app/src/main/java/com/matheussilas97/sensei/MyApplication.kt
@@ -3,11 +3,14 @@ package com.matheussilas97.sensei
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 
 class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = applicationContext
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
     companion object {
         @SuppressLint("StaticFieldLeak")
