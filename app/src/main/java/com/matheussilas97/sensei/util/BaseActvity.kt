9efd1b00b010d3f28fe3bb1f2c90318849ab0225
Matheus-Silas97 <matheus.silas123@gmail.com>
2021-06-20 12:36:58 -0300

    Adicionado código de auxilio na Base activity.

--- app/src/main/java/com/matheussilas97/sensei/util/BaseActvity.kt
+++ app/src/main/java/com/matheussilas97/sensei/util/BaseActvity.kt
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.matheussilas97.sensei.adapter.NoResultAdapter
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 abstract class BaseActvity : AppCompatActivity() {
 
@@ -22,5 +26,34 @@ abstract class BaseActvity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = NoResultAdapter(context, message)
         recyclerView.layoutManager = layoutManager
+    }
+
+    open fun emailValidator(email: String?): Boolean {
+        val pattern: Pattern
+        val emailVal = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+        pattern = Pattern.compile(emailVal)
+        val matcher: Matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+
+    open fun convertDateFormat(
+        date: String?,
+        initDateFormat: String?,
+        endDateFormat: String?
+    ): String? {
+        return try {
+            val initDate = SimpleDateFormat(initDateFormat).parse(date)
+            val formatter = SimpleDateFormat(endDateFormat)
+            formatter.format(initDate)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            "Erro ao obter data"
+        }
     }
 }
